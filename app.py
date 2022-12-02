0dbf79b983252a2cebef33ed426b615717881f7a
William Zhang <williamhyzhang@gmail.com>
2022-12-01 22:35:14 -0500

    feat: config and helper functions

--- app.py
+++ app.py
@@ -6,9 +6,26 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+# configure application
 app = Flask(__name__)
 
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///pguide.db")
+
+@app.after_request
+def after_request(response):
+    """ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
-def hello_world():
+def index():
     return render_template("admin.html")
 
